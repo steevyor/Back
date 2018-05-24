81b2c464118da1c229041622550f1b95d493a6fc
Alex B <erdnaxelasiadrob@gmail.com>
2018-05-24 13:32:06 +0200

    entity complete

--- src/main/java/com/dant/entity/FriendList.java
+++ src/main/java/com/dant/entity/FriendList.java
@@ -1,4 +1,26 @@
 package com.dant.entity;
 
+import java.util.ArrayList;
+
 public class FriendList {
+
+
+    public ArrayList<User> List ;
+
+    public FriendList(){
+        this.List = new ArrayList<User>();
+    }
+
+    public void addFriend(User user){
+        List.add(user);
+    }
+
+    public void removeFriend (User user){
+        List.remove(user);
+    }
+
+    public ArrayList getList() {
+        return List;
+    }
+
 }
