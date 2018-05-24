9e4d3cb0fd04a444df9b8392db47eba1a7886b0a
drheinrich940 <drheinrich940@gmail.com>
2018-05-24 15:18:57 +0200

    UserDAO and CoordinatesDAO updates

--- src/main/java/com/dant/database/CoordinateDAO.java
+++ src/main/java/com/dant/database/CoordinateDAO.java
@@ -4,6 +4,7 @@ import com.dant.entity.Coordinate;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
@@ -27,20 +28,47 @@ public class CoordinateDAO implements DAO<Coordinate> {
             double x = result.getDouble("xCoordinates");
             double y = result.getDouble("yCoordinates");
             return new Coordinate(x,y);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
+            return null;
         }
-        return new Coordinate();
     }
 
     @Override
     public void delete(Coordinate object) {
+        try (Statement st = connection.createStatement()) {
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void setNull(String key){
+        try (Statement st = connection.createStatement()) {
+            st.execute("UPDATE user SET xCoordinates = 0, yCoordinates = 0 FROM user WHERE pseudo =" +key +";");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Coordinate update(Coordinate object) {
-        return null;
+        Coordinate coord = new Coordinate();
+        try (Statement st = connection.createStatement()) {
+            
+        }catch(SQLException e){
+        }
+        return new Coordinate();
+    }
+
+    public Coordinate update(String key){
+        try (Statement st = connection.createStatement()) {
+
+        }catch(SQLException e){
+
+        }
+
+        return new Coordinate();
     }
 
     @Override
