620e5f4824d19c30a28a6c7a9a11661e7d3896c3
Alex B <erdnaxelasiadrob@gmail.com>
2018-05-24 15:55:41 +0200

    Merge branch 'master' of https://github.com/steevyor/Back
    # Conflicts:
    #	src/main/java/com/dant/security/TokenGenerator.java

--- src/main/java/com/dant/security/crypting.java
+++ src/main/java/com/dant/security/crypting.java
@@ -1,20 +1,29 @@
 package com.dant.security;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class crypting {
 
 
     protected String pwd="";
 
-    public crypting(String pwd, String key) {
-        this.setEncrypt(pwd,key);
+    public crypting(String pwd) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        this.setEncrypt(pwd);
     }
 
-    public String setEncrypt(String password, String key){
+    public void setEncrypt(String password) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+    /*
+        String Key = password;
+        byte[] KeyData = Key.getBytes();
+        SecretKeySpec KS = new SecretKeySpec(KeyData, "Blowfish");
+        Cipher cipher = Cipher.getInstance("Blowfish");
+        cipher.init(Cipher.ENCRYPT_MODE, KS);
+    */
 
+
+    /*
         try
         {
             Key clef = new SecretKeySpec(key.getBytes("UTF-8"),"Blowfish");
@@ -26,12 +35,26 @@ public class crypting {
         {
             return null;
         }
+    */
 
-        /*for (int i=0; i<password.length();i++)  {
+
+        for (int i=0; i<password.length();i++)  {
             int c=password.charAt(i)^48;
             this.pwd=this.pwd+(char)c;
-        }*/
+        }
+
     }
+
+
+    public void deCrypt(){
+        String aCrypter= "";
+        for (int i=0; i<this.pwd.length();i++)  {
+            int c=this.pwd.charAt(i)^48;
+            aCrypter=aCrypter+(char)c;
+        }
+    }
+
+
 
     public String getPwd() {
         return pwd;
