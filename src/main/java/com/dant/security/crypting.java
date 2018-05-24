ac19ca461f6ca855db6a9385f1a325d12e8384e3
Alex B <erdnaxelasiadrob@gmail.com>
2018-05-24 15:26:17 +0200

    Merge branch 'master' of https://github.com/steevyor/Back
    # Conflicts:
    #	src/main/java/com/dant/security/TokenGenerator.java

--- src/main/java/com/dant/security/crypting.java
+++ src/main/java/com/dant/security/crypting.java
@@ -1,15 +1,32 @@
 package com.dant.security;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+
 public class crypting {
 
 
     protected String pwd="";
 
-    public crypting(String pwd) {
-        this.setEncrypt(pwd);
+    public crypting(String pwd, String key) {
+        this.setEncrypt(pwd,key);
     }
 
-    public void setEncrypt(String password){
+    public String setEncrypt(String password, String key){
+
+        try
+        {
+            Key clef = new SecretKeySpec(key.getBytes("UTF-8"),"Blowfish");
+            Cipher cipher=Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.ENCRYPT_MODE,clef);
+            return new String(cipher.doFinal(password.getBytes()));
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+
         /*for (int i=0; i<password.length();i++)  {
             int c=password.charAt(i)^48;
             this.pwd=this.pwd+(char)c;
