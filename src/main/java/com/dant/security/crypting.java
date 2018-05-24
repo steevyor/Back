9cb41ba72047e88ce67e0323abf11d3ac5d1e13b
Alex B <erdnaxelasiadrob@gmail.com>
2018-05-24 15:09:57 +0200

    Merge branch 'master' of https://github.com/steevyor/Back
    # Conflicts:
    #	src/main/java/com/dant/security/TokenGenerator.java

--- src/main/java/com/dant/security/crypting.java
+++ src/main/java/com/dant/security/crypting.java
@@ -1,7 +1,24 @@
 package com.dant.security;
 
-public class Crypting {
+public class crypting {
 
+
+    protected String pwd="";
+
+    public crypting(String pwd) {
+        this.setEncrypt(pwd);
+    }
+
+    public void setEncrypt(String password){
+        for (int i=0; i<password.length();i++)  {
+            int c=password.charAt(i)^48;
+            this.pwd=this.pwd+(char)c;
+        }
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
 
 
 }
